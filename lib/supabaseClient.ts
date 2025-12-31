cd49182eebec0aec7f78be670da11d120c1063b9
Rox voices ai <simubalishawn2006@gmail.com>
2025-12-31 07:31:58 +0200

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://huysermpjlasdenslxiy.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1eXNlcm1wamxhc2RlbnNseGl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMDY2NDUsImV4cCI6MjA4MjY4MjY0NX0.OZBI_zM-aEENeFLSCyigo7-G_MpeCCSMtucslDrLxoI";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
